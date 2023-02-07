f61f8d3f757027a5298cb2dbb0352a4f05334e9c
Artyom Bekkuzhinov <bekkygi27@gmail.com>
2023-02-07 15:00:58 +0500

    1.1.2 Межпоточный диалог со счетчиком

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,48 +1,58 @@
 import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
-
-Runnable textProcessor = ()->{
-    long startTs = System.currentTimeMillis();
-    String text = Main.generateText("aab", 30_000);
-    int maxSize = 0;
-    for (int i = 0; i < text.length(); i++) {
-        for (int j = 0; j < text.length(); j++) {
-            if (i >= j) {
-                continue;
-            }
-            boolean bFound = false;
-            for (int k = i; k < j; k++) {
-                if (text.charAt(k) == 'b') {
-                    bFound = true;
-                    break;
+        /*
+        В задании было сказано не менять скопированный код. Не уверен,
+        распространяется ли это на второе задание, на всякий случай оставил
+        */
+        Callable<Integer> textProcessor = () -> {
+            long startTs = System.currentTimeMillis();
+            String text = Main.generateText("aab", 30_000);
+            int maxSize = 0;
+            for (int i = 0; i < text.length(); i++) {
+                for (int j = 0; j < text.length(); j++) {
+                    if (i >= j) {
+                        continue;
+                    }
+                    boolean bFound = false;
+                    for (int k = i; k < j; k++) {
+                        if (text.charAt(k) == 'b') {
+                            bFound = true;
+                            break;
+                        }
+                    }
+                    if (!bFound && maxSize < j - i) {
+                        maxSize = j - i;
+                    }
                 }
             }
-            if (!bFound && maxSize < j - i) {
-                maxSize = j - i;
+            System.out.println(text.substring(0, 100) + " -> " + maxSize);
+            long endTs = System.currentTimeMillis(); // end time
+
+            System.out.println("Time: " + (endTs - startTs) + "ms");
+            return maxSize;
+        };
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        List<Future<Integer>> futures = new ArrayList<>();
+        for (int i = 0; i < texts.length; i++) {
+            futures.add(threadPool.submit(textProcessor));
+        }
+        int max = 0;
+        for (Future<Integer> future : futures) {
+            int f = future.get();
+            if (max < f) {
+                max = f;
             }
         }
-    }
-    System.out.println(text.substring(0, 100) + " -> " + maxSize);
-    long endTs = System.currentTimeMillis(); // end time
-
-    System.out.println("Time: " + (endTs - startTs) + "ms");
-};
-
-        List<Thread> threads = new ArrayList<>();
-        for (int i = 0; i < texts.length; i++) {
-            threads.add(new Thread(textProcessor));
-        }
-        threads.forEach(Thread::start);
-        for (Thread thread : threads) {
-            thread.join();
-        }
+        System.out.println(max);
         long startTs = System.currentTimeMillis(); // start time
         for (String text : texts) {
             int maxSize = 0;
